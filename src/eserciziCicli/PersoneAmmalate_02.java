13297f182698c6a079852b2b33efd36241e9f8b7
18531 <18531@W10STAT-7-156.EDU.MARCONI>
2020-11-10 09:26:50 +0100

    PersoneAmmalate_02

--- src/eserciziCicli/PersoneAmmalate_02.java
+++ src/eserciziCicli/PersoneAmmalate_02.java
@@ -3,13 +3,15 @@ package eserciziCicli;
 
 
 public class PersoneAmmalate_02 {
-    
-    
-    
     public static void main(String[] args) {
-    
-        
-        
-        
+        int ammalati = 1000,contGiorni = 0;
+        int ammalatiSout = ammalati;
+        double percGuariti = 20.0;
+        while(ammalati > 100){
+           double guariti = (double)ammalati / 100 * percGuariti;
+           ammalati -= guariti;
+           contGiorni += 1;
+        }
+        System.out.println("per guarire gli ammalati ci sono voluti "+contGiorni+" giorni");
     }
 }
