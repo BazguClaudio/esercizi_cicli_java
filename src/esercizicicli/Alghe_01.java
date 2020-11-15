bb2f8dd689584d33b9ec62d9fdd4744b737ff0a8
Claudio <18531@studenti.marconiverona.edu.it>
2020-11-15 13:23:30 +0100

    Alghe_01

--- src/esercizicicli/Alghe_01.java
+++ src/esercizicicli/Alghe_01.java
@@ -3,22 +3,21 @@ package esercizicicli;
 public class Alghe_01 {
 
     public static void main(String[] args) {
-        int countAnni = 1, popolazioneIniziale = 9, popolazioneFinale = 12;
+        int countAnni = 0, popolazioneIniziale = 9, popolazioneFinale = 12;
         boolean conferma = true;
-        do {
+        while (conferma) {
+            countAnni += 1;
             if (countAnni % 2 == 1) {
                 popolazioneIniziale *= 2;
             }
-            if (countAnni % 2 == 0 ) {
-                int calcolopopolazioneIniziale = popolazioneIniziale/3;
-                popolazioneIniziale -= calcolopopolazioneIniziale;
+            if (countAnni % 2 == 0) {
+                int calcoloPopolazioneIniziale = popolazioneIniziale / 3;
+                popolazioneIniziale -= calcoloPopolazioneIniziale;
             }
-            countAnni += 1;
             if (popolazioneIniziale == popolazioneFinale) {
                 conferma = false;
             }
-
-        } while (conferma);
-        System.out.println("ci sono voluti: "+ countAnni+" anni." );
+        }
+        System.out.println("ci sono voluti: " + countAnni + " anni.");
     }
 }
