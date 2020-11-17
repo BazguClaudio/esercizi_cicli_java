230b700b6939406c41442935917c64e3d00314fa
18531 <18531@W10STAT-7-156.EDU.MARCONI>
2020-11-17 08:36:36 +0100

    FattoriPrimi_01

--- src/eserciziCicli/FattoriPrimi_01.java
+++ src/eserciziCicli/FattoriPrimi_01.java
@@ -2,11 +2,15 @@ package eserciziCicli;
 
 public class FattoriPrimi_01 {
     public static void main(String[] args){
-        int num = 12,i = 2;
-        while(num % i != 0){
+        int num = 30,i = 2,numI = 1;
+        boolean conferma = true;
+        while(conferma){
             if(num % i == 0){
-                int resto = num % i;
-                System.out.println(resto);
+                num = num / i;
+                System.out.println(i);
+                if(num == 1){
+                    conferma = false;
+                }
             }
             else if(num % i != 0){
                 i+=1;
